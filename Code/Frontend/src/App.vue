c4d58cfba97eef6d6460bfbf70e5958a2f5457a2
Raphi <raphael.antonietti@liip.ch>
2019-08-24 01:53:34 +0200

    Initial setup

---
+++ Code/Frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col" id="app">
+    <div class="mb-12" id="nav"></div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
