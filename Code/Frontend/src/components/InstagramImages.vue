6f6d8b43f3b89b7d5374c5fbfdf88acf4c07c240
Raphi <raphael.antonietti@liip.ch>
2019-08-24 12:05:27 +0200

    Implement scroll overflow and stylings on ig

--- Code/Frontend/src/components/InstagramImages.vue
+++ Code/Frontend/src/components/InstagramImages.vue
@@ -1,18 +1,13 @@
 <template>
-  <div v-if="images">
-    <img
-      v-for="(image, index) in images"
-      :key="'igimage' + hashtag + index"
-      :src="image"
-      :alt="'instagram image of ' + hashtag"
-    />
+  <div class="ig-image-slider" v-if="images">
+    <img class="ig-image" v-for="(image, index) in images" :key="'igimage' + hashtag + index" :src="image" :alt="'instagram image of ' + hashtag" />
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 export default {
-  name: "InstagramImages",
+  name: 'InstagramImages',
   data() {
     return {
       images: null
@@ -21,17 +16,23 @@ export default {
   props: {
     hashtag: {
       type: String,
-      default: "gornergrat"
+      default: 'gornergrat'
     }
   },
   created() {
-    axios
-      .get("http://5.9.112.47:1569/instagram?hashtag=" + this.hashtag)
-      .then(result => {
-        this.images = result.data;
-      });
+    axios.get('http://5.9.112.47:1569/instagram?hashtag=' + this.hashtag).then(result => {
+      this.images = result.data;
+    });
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped>
+.ig-image-slider {
+  @apply flex overflow-scroll;
+}
+.ig-image {
+  @apply min-w-full;
+  height: auto;
+}
+</style>
