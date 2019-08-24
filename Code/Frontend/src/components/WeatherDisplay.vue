a07da745f37b2481ef8c804a9180d276bbfc5281
Raphi <raphael.antonietti@liip.ch>
2019-08-24 11:30:40 +0200

    Intial axios setup and weather component

---
+++ Code/Frontend/src/components/WeatherDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "WeatherDisplay",
+  data() {
+    return {
+      weather: "sunny"
+    };
+  },
+  created() {
+    this.weather =
+      localStorage.getItem("weather") || this.getWeather() || "sunny";
+  },
+  methods: {
+    getWeather() {
+      axios
+        .get(
+          "https://api.darksky.net/forecast/5375e67e1f87323c4b9b9ef18c29f5db/46.020714,7.749117"
+        )
+        .then(result => {
+          console.log(result);
+
+          debugger;
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- Code/Frontend/src/views/Home.vue
+++ Code/Frontend/src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home p-5 pt-0 mb-6">
+    <WeatherDisplay />
     Home!
   </div>
 </template>
 
 <script>
+import WeatherDisplay from "../components/WeatherDisplay.vue";
 export default {
   name: "home",
-  components: {}
+  components: { WeatherDisplay }
 };
 </script>
 
