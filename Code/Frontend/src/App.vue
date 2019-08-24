f96a1aa5adf18befa787df639032ee965e07648a
Raphi <raphael.antonietti@liip.ch>
2019-08-24 16:23:53 +0200

    Make everything myriad pro

--- Code/Frontend/src/App.vue
+++ Code/Frontend/src/App.vue
@@ -14,9 +14,7 @@
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  @apply font-sans;
 }
 </style>
