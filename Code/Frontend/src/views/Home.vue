5a55b2679f26c7d597503ae2a0d7a0a83157be95
Raphi <raphael.antonietti@liip.ch>
2019-08-24 11:56:06 +0200

    embed Instagram embed

--- Code/Frontend/src/views/Home.vue
+++ Code/Frontend/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home p-5 pt-0 mb-6">
     <WeatherDisplay />
+    <InstagramImages hashtag="zermatt" />
     Home!
   </div>
 </template>
 
 <script>
+import InstagramImages from "../components/InstagramImages.vue";
 import WeatherDisplay from "../components/WeatherDisplay.vue";
 export default {
   name: "home",
-  components: { WeatherDisplay }
+  components: { WeatherDisplay, InstagramImages }
 };
 </script>
 
